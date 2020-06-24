5c7646d665b93dc4c7276aa68328a54ca06eaa01
Nathan Chung <chung.nathanh@gmail.com>
2020-06-24 01:46:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,7 +18,8 @@ def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
   hash.each do |key, value| 
-    if lowest_value == nil || value < lowest_value 
+    if lowest_value == nil or
+      value < lowest_value 
       lowest_value = value
       lowest_key = key
     end 
